5e6cabce2ae82452c5c1848aef66882d701d230e
JLVazquez22 <vazaceitunojoseluis@gmail.com>
2025-02-18 13:07:40 +0100

    Prueba

--- ausospa/index.php
+++ ausospa/index.php
@@ -24,12 +24,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <body class="bg-[#E0E0E0] flex items-center justify-center h-screen w-screen">
     <div class="flex flex-col md:flex-row bg-white rounded-lg shadow-lg w-full h-full">
         <!-- Imagen independiente -->
-        <div class="img__perro w-full md:w-1/2 flex justify-center items-start bg-gray-200">
+        <div class="img__perro w-full md:w-1/2 flex justify-center items-start">
             <h1 class="text-center text-xl mt-6">Bienvenido a <span class="text-3xl ml-1 font-bold"> SpaRibera</span></h1>
         </div>
         <!-- Contenedor del formulario -->
-        <div class="w-full md:w-1/2 flex flex-col justify-center p-8">
-            <div class="bg-[#E0E0E0] text-white p-8 rounded-lg shadow-lg w-full">
+        <div class="w-full md:w-1/2 flex flex-col justify-center items-center p-8">
+            <div class="bg-[#E0D1D3] text-white p-8 rounded-lg shadow-lg border-2 border-black w-2/3">
                 <h2 class="text-2xl font-bold text-center text-black mb-6">Iniciar sesión</h2>
                 <form action="" method="POST" class="space-y-4">
                     <div>
@@ -40,7 +40,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         <label for="password" class="block text-black">Contraseña</label>
                         <input type="password" id="password" name="password" class="w-full px-4 py-2 mt-2 border rounded-lg focus:ring-2 focus:ring-blue-400" required>
                     </div>
-                    <button type="submit" class="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-950 transition">Ingresar</button>
+                    <!-- AGREGAR OLVIDASTE CONTRASEÑA Y ICONS DE PERROS -->
+                    <p class="text-sm text-gray-600 text-center hover:text-blue-500 transition">¿Olvidaste tu contraseña?</p>
+                    <button type="submit" class="w-full bg-black text-white py-2 mb-3 rounded-lg hover:bg-gray-950 transition">Ingresar</button>
+                    <hr class="my-4 border-black">
+                    <p class="text-sm text-gray-600 text-center">Hazte socio para disfrutar de numerosas ventajas... <a href="#" class="text-blue-500 hover:underline">Registrarse!</a></p>
                 </form>
             </div>
         </div>
